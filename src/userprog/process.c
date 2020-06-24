49f742d02d08f45e61521ccbd883808eaef418f4
furymonkey <furymonkey@naver.com>
2020-06-24 19:58:00 +0900

    matching prev declaration and usage of function

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -208,7 +208,7 @@ struct Elf32_Phdr
 #define PF_W 2          /* Writable. */
 #define PF_R 4          /* Readable. */
 
-static bool setup_stack (void **esp);
+static bool setup_stack(void** esp, char* file_name);
 static bool validate_segment (const struct Elf32_Phdr *, struct file *);
 static bool load_segment (struct file *file, off_t ofs, uint8_t *upage,
                           uint32_t read_bytes, uint32_t zero_bytes,
@@ -317,7 +317,7 @@ load (const char *file_name, void (**eip) (void), void **esp)
     }
 
   /* Set up stack. */
-  if (!setup_stack (esp))
+  if (!setup_stack(esp, file_name))
     goto done;
 
   /* Start address. */
