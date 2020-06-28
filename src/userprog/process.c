94637af1e0ab3c2aab1af4f47b9315dc02aa6068
Seungyoung Lee <superpowerex@naver.com>
2020-06-28 19:03:09 +0900

    change test code

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -484,14 +484,14 @@ void set_esp(void** esp, char* file_name)
 
         args_addr[i] = user_addr;
 
-        printf("args[%d] : %s", i, args[i], size);
+        printf("args[%d] : %s\n", i, args[i], size);
     }
 
     int word_align = user_addr % WORD_SIZE;
+    printf("word_algin : %d\n", word_align);
 
     if (word_align > 0)
     {
-        printf("word_algin : %d\n", word_align);
         user_addr -= word_align;
     }
 
