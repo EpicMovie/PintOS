118d63a381f38dc589b07be74f59939c0dcc47cb
Seungyoung Lee <superpowerex@naver.com>
2020-06-28 18:30:22 +0900

    syscall 구현 시작

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -15,7 +15,126 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf("syscall : %d\n", *(uint32_t*)(f->esp));
   printf ("system call!\n");
-  thread_exit ();
+
+  hex_dump(*f->esp, *f->esp, 200, true);
+
+  /*
+  switch (*(uint32_t*)(f->esp))
+  {
+    case SYS_HALT:
+        halt();
+        break;
+    case SYS_EXIT:
+        exit();
+        break;
+    case SYS_EXEC:
+        exec();
+        break;
+    case SYS_WAIT:
+        wait();
+        break;
+    case SYS_CREATE:
+        create();
+        break;
+    case SYS_REMOVE:
+        remove();
+        break;
+    case SYS_OPEN:
+        open();
+        break;
+    case SYS_FILESIZE:
+        filesize();
+        break;
+    case SYS_READ:
+        read();
+        break;
+    case SYS_WRITE:
+        write();
+        break;
+    case SYS_SEEK:
+        seek();
+        break;
+    case SYS_TELL:
+        tell();
+        break;
+    case SYS_CLOSE:
+        close();
+        break;
+    default:
+        break;
+  }
+  */
 }
+
+void halt(void)
+{
+    shutdown_power_off();
+}
+
+void exit(int status)
+{
+	thread_exit();
+}
+
+pid_t exec(const char* file)
+{
+
+}
+
+int wait(pid_t)
+{
+
+}
+
+bool create(const char* file, unsigned initial_size)
+{
+
+}
+
+bool remove(const char* file)
+{
+
+}
+
+int open(const char* file)
+{
+
+}
+
+int filesize(int fd)
+{
+
+}
+
+int read(int fd, void* buffer, unsigned length)
+{
+
+}
+
+int write(int fd, const void* buffer, unsigned length)
+{
+    if (fd == 1)
+    {
+        putbuf((char*)buffer, length);
+        return length;
+    }
+
+    return 0;
+}
+
+void seek(int fd, unsigned position)
+{
+
+}
+
+unsigned tell(int fd)
+{
+
+}
+
+void close(int fd)
+{
+
+}
+
